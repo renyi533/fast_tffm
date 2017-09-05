7f74e23c6f1ef2834bbdeb8cbacd4f4c24f564a8
hengrui.ry <hengrui.ry@alibaba-inc.com>
2017-09-05 19:25:37 +0800

    test

--- cc/eve_alg.cc
+++ cc/eve_alg.cc
@@ -4,17 +4,21 @@
 #include <algorithm>
 #include "tensorflow/core/framework/register_types.h"
 #include "tensorflow/core/kernels/bounds_check.h"
-#include <math.h> 
+#include <math.h>
+#include <iostream>
 
 REGISTER_OP("EveAlg")
     .Input("f: float32")
+    .Input("smoothed_f: float32")
     .Input("d: float32")
     .Input("target: float32")
     .Input("step: int64")
     .Input("decay_rate: float32")
+    .Input("f_decay_rate: float32")
     .Input("lower: float32")
     .Input("upper: float32")
     .Output("out_f: float32")
+    .Output("out_smoothed_f: float32")
     .Output("out_d: float32");
 
 using namespace tensorflow;
@@ -27,6 +31,8 @@ class EveAlgOp : public OpKernel {
   void Compute(OpKernelContext* ctx) override {
     const Tensor* f;
     OP_REQUIRES_OK(ctx, ctx->input("f", &f));
+    const Tensor* smoothed_f;
+    OP_REQUIRES_OK(ctx, ctx->input("smoothed_f", &smoothed_f));
     const Tensor* d;
     OP_REQUIRES_OK(ctx, ctx->input("d", &d));
     const Tensor* target;
@@ -35,6 +41,8 @@ class EveAlgOp : public OpKernel {
     OP_REQUIRES_OK(ctx, ctx->input("step", &step));
     const Tensor* decay_rate;
     OP_REQUIRES_OK(ctx, ctx->input("decay_rate", &decay_rate));
+    const Tensor* f_decay_rate;
+    OP_REQUIRES_OK(ctx, ctx->input("f_decay_rate", &f_decay_rate));
     const Tensor* lower;
     OP_REQUIRES_OK(ctx, ctx->input("lower", &lower));
     const Tensor* upper;
@@ -44,6 +52,10 @@ class EveAlgOp : public OpKernel {
                 errors::InvalidArgument("f is not a scalar: ",
                                         f->shape().DebugString()));
 
+    OP_REQUIRES(ctx, TensorShapeUtils::IsScalar(smoothed_f->shape()),
+                errors::InvalidArgument("smoothed_f is not a scalar: ",
+                                        smoothed_f->shape().DebugString()));
+ 
     OP_REQUIRES(ctx, TensorShapeUtils::IsScalar(d->shape()),
                 errors::InvalidArgument("d is not a scalar: ",
                                         d->shape().DebugString()));
@@ -60,6 +72,10 @@ class EveAlgOp : public OpKernel {
                 errors::InvalidArgument("decay_rate is not a scalar: ",
                                         decay_rate->shape().DebugString()));
 
+    OP_REQUIRES(ctx, TensorShapeUtils::IsScalar(f_decay_rate->shape()),
+                errors::InvalidArgument("f_decay_rate is not a scalar: ",
+                                        f_decay_rate->shape().DebugString()));
+
     OP_REQUIRES(ctx, TensorShapeUtils::IsScalar(lower->shape()),
                 errors::InvalidArgument("lower is not a scalar: ",
                                         lower->shape().DebugString()));
@@ -69,24 +85,37 @@ class EveAlgOp : public OpKernel {
                                         upper->shape().DebugString()));
 
     float prev_target = f->scalar<float>()();
+    float smoothed_target = smoothed_f->scalar<float>()();
     float prev_d = d->scalar<float>()();
     float curr_target = target->scalar<float>()();
     float curr_step = step->scalar<int64>()();
     float beta = decay_rate->scalar<float>()();
+    float f_beta = f_decay_rate->scalar<float>()();
     float low = lower->scalar<float>()();
     float up = upper->scalar<float>()();
 
     Tensor* out_f;
     OP_REQUIRES_OK(ctx, ctx->allocate_output("out_f", TensorShape({}), &out_f));
+    Tensor* out_smoothed_f;
+    OP_REQUIRES_OK(ctx, ctx->allocate_output("out_smoothed_f", TensorShape({}), &out_smoothed_f));
     Tensor* out_d;
     OP_REQUIRES_OK(ctx, ctx->allocate_output("out_d", TensorShape({}), &out_d));
 
     auto f_ref = out_f->flat<float>();
     auto d_ref = out_d->flat<float>();
+    auto smoothed_f_ref = out_smoothed_f->flat<float>();
     
     if (step == 0 || prev_target<=1e-6) {
       f_ref(0) = curr_target;
+      smoothed_f_ref(0) = curr_target;
       d_ref(0) = 1.0;
+      return;
+    }
+    else if (curr_target < 0) {
+      std::cout << "Warning: curr_target:"<<curr_target<<std::endl;
+      f_ref(0) = prev_target;
+      d_ref(0) = prev_d;
+      smoothed_f_ref(0) = smoothed_target;
       return;
     }
 
@@ -104,11 +133,13 @@ class EveAlgOp : public OpKernel {
 
     float c = std::min(std::max(ratio_low, curr_target/prev_target), ratio_up);
     curr_target = c * prev_target;
-    float r = fabs(curr_target - prev_target) / std::min(curr_target, prev_target);
+    float min_target = std::min(curr_target, prev_target);
+    smoothed_target = smoothed_target * f_beta + min_target * (1 - f_beta);
+    float r = fabs(curr_target - prev_target) / smoothed_target;
     
     d_ref(0) = beta * prev_d + (1 - beta) * r;
     f_ref(0) = curr_target;
-
+    smoothed_f_ref(0) = smoothed_target;
   }
  private:
 };
--- py/eve_operations.py
+++ py/eve_operations.py
@@ -4,7 +4,7 @@ from tensorflow.python.framework import ops
 
 eve_alg_op = tf.load_op_library(os.path.dirname(os.path.realpath(__file__)) + '/libeve_alg_op.so')
 
-def eve_rate(curr_target, decay_rate, lower, upper, is_local_mode, worker_idx):
+def eve_rate(curr_target, decay_rate, f_decay_rate, lower, upper, is_local_mode, worker_idx):
   device_str = "/cpu:0"
   var_collection = None
   if not is_local_mode:
@@ -17,17 +17,20 @@ def eve_rate(curr_target, decay_rate, lower, upper, is_local_mode, worker_idx):
   with tf.device(device_str), tf.name_scope("eve_scope"):
     d = tf.Variable(1.0, trainable=False, dtype=tf.float32, name="d", collections=var_collection)
     f = tf.Variable(0.0, trainable=False, dtype=tf.float32, name="f", collections=var_collection)
+    smoothed_f = tf.Variable(0.0, trainable=False, dtype=tf.float32, name="smoothed_f", collections=var_collection)
     step = tf.Variable(0, trainable=False, dtype=tf.int64, name="step", collections=var_collection)
 
   up = tf.convert_to_tensor(upper)
   low = tf.convert_to_tensor(lower)
   decay = tf.convert_to_tensor(decay_rate)
+  f_decay = tf.convert_to_tensor(f_decay_rate)
 
-  new_f, new_d = eve_alg_op.eve_alg(f, d, curr_target, step, decay, low, up)
+  new_f, new_smoothed_f, new_d = eve_alg_op.eve_alg(f, smoothed_f, d, curr_target, step, decay, f_decay, low, up)
 
   f_assign = tf.assign(f, new_f)
+  smoothed_f_assign = tf.assign(smoothed_f, new_smoothed_f)
   step_inc = tf.assign_add(step, 1)
-  with tf.control_dependencies([f_assign, step_inc]):
+  with tf.control_dependencies([f_assign, step_inc, smoothed_f_assign]):
     d_assign = tf.assign(d, new_d)
-  return d_assign
+  return d_assign, f_assign, smoothed_f_assign
 
